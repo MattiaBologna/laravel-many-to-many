b7fa9ad0d1fd6b727fa75d91e628c096db921f30
MattiaBologna <mattiabologna01@gmail.com>
2024-06-08 17:44:49 +0200

    Add: project_technology relation seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,7 +19,9 @@ class ProjectSeeder extends Seeder
     {
 
         $types = Type::all();
-        $ids = $types->pluck('id')->all();
+        $type_ids = $types->pluck('id')->all();
+
+        $technology_ids = Technology::all()->pluck('id')->all();
 
         for ($i = 0; $i < 10; $i++) {
 
@@ -27,10 +30,14 @@ class ProjectSeeder extends Seeder
             $project->title = $faker->sentence(6);
             $project->slug = Str::slug($project->title);
             $project->description = $faker->text(50);
-            $project->type_id = $faker->optional()->randomElement($ids);
+            $project->type_id = $faker->optional()->randomElement($type_ids);
             $project->link = 'https://github.com/MattiaBologna/laravel-dc-comics.git';
 
             $project->save();
+
+            $random_technology_ids = $faker->randomElements($technology_ids, null);
+            // dopo che il progetto è stato salvato posso recuperare l'id per collegarlo alle tecnologie
+            $project->technologies()->attach($random_technology_ids);
         }
     }
 }
